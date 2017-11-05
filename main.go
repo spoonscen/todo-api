c2ddcfba4e13b7aa08cf69de089ac4613a3700b3
Cory <cory.noonan@gmail.com>
2017-11-05 16:36:32 -0500

    get docs into database and handle errors

--- main.go
+++ main.go
@@ -4,17 +4,33 @@ import (
 	"fmt"
 	"html"
 	"net/http"
+	"time"
 
 	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
+type Todo struct {
+	ID        bson.ObjectId `bson:"_id,omitempty"`
+	Text      string
+	Completed bool
+	Timestamp time.Time
+}
+
+func errorHandler(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
-	url := "localhost:27017"
+	url := "mongo"
 	session, err := mgo.Dial(url)
-	fmt.Println(err)
+	errorHandler(err)
 
 	c := session.DB("todo_api").C("todos")
-	fmt.Println(c)
+	err = c.Insert(&Todo{Text: "Test", Completed: false, Timestamp: time.Now()})
+	errorHandler(err)
 
 	port := "8070"
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
