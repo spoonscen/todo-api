9dd9d887757c4f4507c00d68935fdadb2e939196
Cory <cory.noonan@gmail.com>
2017-11-05 18:20:09 -0500

    use http lib and create and getById

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"html"
 	"net/http"
 	"time"
 
+	"github.com/julienschmidt/httprouter"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
@@ -23,19 +24,81 @@ func errorHandler(err error) {
 	}
 }
 
-func main() {
+var c *mgo.Collection
+
+func AllTodos(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	var allTodos []Todo
+	err := c.Find(nil).Sort("-timestamp").All(&allTodos)
+	json, err := json.Marshal(allTodos)
+	errorHandler(err)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(json)
+}
+
+func GetOneById(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	todo := Todo{}
+	id := bson.ObjectIdHex(ps.ByName("id"))
+	err := c.Find(bson.M{"_id": id}).One(&todo)
+	if err != nil {
+		w.WriteHeader(404)
+		fmt.Fprint(w, err.Error())
+	} else {
+		json, err := json.Marshal(todo)
+		errorHandler(err)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(json)
+	}
+}
+
+func CreateTodo(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	var err error
+	var text string
+
+	decoder := json.NewDecoder(r.Body)
+	id := bson.NewObjectId()
+
+	err = decoder.Decode(&text)
+	errorHandler(err)
+
+	todo := &Todo{ID: id, Text: text, Completed: false, Timestamp: time.Now()}
+	err = c.Insert(todo)
+	errorHandler(err)
+
+	json, err := json.Marshal(todo)
+	errorHandler(err)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(json)
+}
+
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Todo API !")
+}
+
+func RunServer() {
+	port := "8070"
+	router := httprouter.New()
+	router.GET("/", Index)
+	router.GET("/api/todos", AllTodos)
+	router.GET("/api/todos/:id", GetOneById)
+	router.POST("/api/todos/create", CreateTodo)
+	fmt.Println(http.ListenAndServe(":"+port, router))
+}
+
+func ConnectToDb() {
 	url := "mongo"
 	session, err := mgo.Dial(url)
 	errorHandler(err)
+	c = session.DB("todo_api").C("todos")
+}
 
-	c := session.DB("todo_api").C("todos")
-	err = c.Insert(&Todo{Text: "Test", Completed: false, Timestamp: time.Now()})
+func InsetMockDocs() {
+	err := c.Insert(&Todo{Text: "Test1", Completed: false, Timestamp: time.Now()}, &Todo{Text: "Test2", Completed: false, Timestamp: time.Now()})
 	errorHandler(err)
+}
 
-	port := "8070"
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Fprintf(writer, "Its Alive!, %q", html.EscapeString(request.URL.Path))
-	})
-	fmt.Println("Listening on " + port)
-	http.ListenAndServe(":"+port, nil)
+func main() {
+	ConnectToDb()
+	InsetMockDocs()
+	RunServer()
 }
